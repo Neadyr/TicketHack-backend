1763869863847b2d2e56190ced427324b5494e6e
Romain Cochonnat <romain.cochonnat@gmail.com>
2024-07-03 10:11:49 +0200

    visibilité password

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   username: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   tripCart: [{ type: mongoose.Schema.Types.ObjectId, ref: "trips" }],
   tripBooked: [{ type: mongoose.Schema.Types.ObjectId, ref: "trips" }],
   tripPassed: [{ type: mongoose.Schema.Types.ObjectId, ref: "trips" }],
